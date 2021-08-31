5552d8b24cd518ed5b837a08c1a8422dec01e14e
Sulfano Agus Fikri <sulfano.fikri@flash-coffee.com>
2021-08-31 22:16:28 +0700

    fix error event fullday

--- src/pages/Dashboard/events/components/EventFormStep3.js
+++ src/pages/Dashboard/events/components/EventFormStep3.js
@@ -9,7 +9,7 @@ import {
   TimeInput,
 } from "components";
 import React from "react";
-import { Col, Row } from "reactstrap";
+import { Badge, Col, Row } from "reactstrap";
 
 export const EventFormStep3 = ({ onFormFieldChange, formData }) => {
   const handleChange = ({ key, value }) => {
@@ -20,8 +20,8 @@ export const EventFormStep3 = ({ onFormFieldChange, formData }) => {
     const qualificationDays = [...formData.qualificationDays];
     qualificationDays[dayIndex].openDetails = true;
     qualificationDays[dayIndex]?.details.push({
-      startTime: "",
-      endTime: "",
+      startTime: "00:00",
+      endTime: "23:59",
       openEdit: true,
       quota: 0,
     });
@@ -130,173 +130,222 @@ export const EventFormStep3 = ({ onFormFieldChange, formData }) => {
               alwaysOpen: true,
               hide: formData.eventType !== "marathon",
               body: (
-                <Row>
-                  <Col lg={3}>
-                    <DateInput
-                      label="Buka Kualifikasi"
-                      name="quatificationStartDatetime"
-                      value={formData.quatificationStartDatetime}
-                      onChange={handleChange}
-                    />
-                  </Col>
-                  <Col lg={3}>
-                    <DateInput
-                      label="Tutup Kualifikasi"
-                      name="quatificationEndDatetime"
-                      value={formData.quatificationEndDatetime}
-                      onChange={handleChange}
-                    />
-                  </Col>
-                  <Col lg={3}>
-                    <SelectInput
-                      label="Sesi"
-                      name="sessionLength"
-                      value={formData.sessionLength}
-                      options={[{ id: "60", label: "Per 1 Jam" }]}
-                      onChange={handleChange}
-                    />
-                  </Col>
-                  <Col lg={3}>
-                    <SwitchInput
-                      name="weekdaysOnly"
-                      value={formData.weekdaysOnly}
-                      onChange={handleChange}
-                      label="Weekday Only"
-                      options={[
-                        {
-                          id: 1,
-                          label: "Weekdays only",
-                        },
-                      ]}
-                    />
-                  </Col>
-                  <Col lg={12}>
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th>Hari</th>
-                          <th>Sesi</th>
-                          <th style={{width: '1%', whiteSpace: 'nowrap'}}>Detail Sesi</th>
-                          <th>Kuota</th>
-                          <th>...</th>
-                        </tr>
-                      </thead>
-                      {formData.qualificationDays.map((day, dayIndex) => (
-                        <tbody key={day.id}>
-                          <tr>
-                            <td>{day.label}</td>
-                            <td>{day.details?.length} Sesi</td>
-                            <td>
-                              {day.details?.[0]?.startTime} -{" "}
-                              {day.details?.[0]?.endTime}
-                            </td>
-                            <td>{sumQuota(day.details)}</td>
-                            <td>
-                              <Button
-                                type="light"
-                                icon="plus-circle"
-                                outline
-                                size="sm"
-                                onClick={() => addSession(dayIndex)}
-                              />
-                              {" "}
-                              <Button
-                                type="light"
-                                icon={
-                                  day.openDetails
-                                    ? "chevron-up"
-                                    : "chevron-down"
-                                }
-                                outline
-                                size="sm"
-                                onClick={() => expandSession(dayIndex)}
-                              />
-                            </td>
-                          </tr>
-                          {day.openDetails &&
-                            day.details.map((detail, detailIndex) => (
-                              <tr
-                                key={detailIndex}
-                                style={{ background: "#F8F9FA" }}
-                              >
-                                <td></td>
-                                <td>Sesi {detailIndex + 1}</td>
-                                <td style={{width: '1%', whiteSpace: 'nowrap'}}>
-                                  {detail.openEdit ? (
-                                    <div style={{ display: "flex" }}>
-                                      <div style={{ width: 140 }}>
-                                        <TimeInput
-                                          name={`qualificationDays.${dayIndex}.details.${detailIndex}.startTime`}
-                                          value={detail.startTime}
-                                          onChange={handleChange}
-                                        />
-                                      </div>
-                                      <div> - </div>
-                                      <div style={{ width: 140 }}>
-                                        <TimeInput
-                                          name={`qualificationDays.${dayIndex}.details.${detailIndex}.endTime`}
-                                          value={detail.endTime}
-                                          onChange={handleChange}
-                                        />
-                                      </div>
-                                    </div>
-                                  ) : (
-                                    `${detail.startTime} - ${detail.endTime}`
-                                  )}
-                                </td>
-                                <td style={{width: '1%'}}>
-                                  {detail.openEdit ? (
-                                    <div style={{ width: 100 }}>
-                                      <NumberInput
-                                        name={`qualificationDays.${dayIndex}.details.${detailIndex}.quota`}
-                                        value={detail.quota}
-                                        onChange={handleChange}
-                                      />
-                                    </div>
-                                  ) : (
-                                    detail.quota
-                                  )}
+                <>
+                  {formData.eventType === "marathon" && (
+                    <Row>
+                      <Col lg={3}>
+                        <DateInput
+                          label="Buka Kualifikasi"
+                          name="quatificationStartDatetime"
+                          value={formData.quatificationStartDatetime}
+                          onChange={handleChange}
+                        />
+                      </Col>
+                      <Col lg={3}>
+                        <DateInput
+                          label="Tutup Kualifikasi"
+                          name="quatificationEndDatetime"
+                          value={formData.quatificationEndDatetime}
+                          onChange={handleChange}
+                        />
+                      </Col>
+                      <Col lg={3}>
+                        <SelectInput
+                          label="Sesi"
+                          name="sessionLength"
+                          value={formData.sessionLength}
+                          options={[{ id: "60", label: "Per 1 Jam" }]}
+                          onChange={handleChange}
+                        />
+                      </Col>
+                      <Col lg={3}>
+                        <SwitchInput
+                          name="weekdaysOnly"
+                          value={formData.weekdaysOnly}
+                          onChange={handleChange}
+                          label="Weekday Only"
+                          options={[
+                            {
+                              id: 1,
+                              label: "Weekdays only",
+                            },
+                          ]}
+                        />
+                      </Col>
+                      <Col lg={12}>
+                        <table className="table">
+                          <thead>
+                            <tr>
+                              <th>Hari</th>
+                              <th>Sesi</th>
+                              <th style={{ width: "1%", whiteSpace: "nowrap" }}>
+                                Detail Sesi
+                              </th>
+                              <th>Kuota</th>
+                              <th style={{ width: "1%", whiteSpace: "nowrap" }}>
+                                ...
+                              </th>
+                            </tr>
+                          </thead>
+                          {formData.qualificationDays.map((day, dayIndex) => (
+                            <tbody key={day.id}>
+                              <tr>
+                                <td>{day.label}</td>
+                                <td>
+                                  <Badge
+                                    className="bg-info"
+                                    style={{ fontSize: "1em" }}
+                                  >
+                                    {day.details?.length} Sesi
+                                  </Badge>
                                 </td>
                                 <td>
-                                  {detail.openEdit ? (
-                                    <Button
-                                      type="primary"
-                                      icon="check"
-                                      outline
-                                      size="sm"
-                                      onClick={() =>
-                                        saveSessionItem(dayIndex, detailIndex)
-                                      }
-                                    />
-                                  ) : (
-                                    <Button
-                                      type="warning"
-                                      icon="pencil"
-                                      outline
-                                      size="sm"
-                                      onClick={() =>
-                                        editSessionItem(dayIndex, detailIndex)
-                                      }
-                                    />
-                                  )}
-                                  {" "}
+                                  {day.details?.[0]?.startTime} -{" "}
+                                  {day.details?.[0]?.endTime}
+                                </td>
+                                <td>
+                                  <Badge
+                                    className="bg-info"
+                                    style={{ fontSize: "1em" }}
+                                  >
+                                    {sumQuota(day.details)}
+                                  </Badge>
+                                </td>
+                                <td
+                                  style={{ width: "1%", whiteSpace: "nowrap" }}
+                                >
                                   <Button
-                                    type="danger"
-                                    icon="trash"
+                                    type="light"
+                                    icon="plus-circle"
                                     outline
                                     size="sm"
-                                    onClick={() =>
-                                      removeSessionItem(dayIndex, detailIndex)
+                                    onClick={() => addSession(dayIndex)}
+                                  />{" "}
+                                  <Button
+                                    type="light"
+                                    icon={
+                                      day.openDetails
+                                        ? "chevron-up"
+                                        : "chevron-down"
                                     }
+                                    outline
+                                    size="sm"
+                                    onClick={() => expandSession(dayIndex)}
                                   />
                                 </td>
                               </tr>
-                            ))}
-                        </tbody>
-                      ))}
-                    </table>
-                  </Col>
-                </Row>
+                              {day.openDetails &&
+                                day.details.map((detail, detailIndex) => (
+                                  <tr
+                                    key={detailIndex}
+                                    style={{ background: "#F8F9FA" }}
+                                  >
+                                    <td></td>
+                                    <td>Sesi {detailIndex + 1}</td>
+                                    <td
+                                      style={{
+                                        width: "1%",
+                                        whiteSpace: "nowrap",
+                                      }}
+                                    >
+                                      {detail.openEdit ? (
+                                        <div style={{ display: "flex" }}>
+                                          <div style={{ width: 140 }}>
+                                            <TimeInput
+                                              name={`qualificationDays.${dayIndex}.details.${detailIndex}.startTime`}
+                                              value={detail.startTime}
+                                              onChange={handleChange}
+                                            />
+                                          </div>
+                                          <div
+                                            style={{
+                                              width: 20,
+                                              textAlign: "center",
+                                              paddingTop: 10,
+                                            }}
+                                          >
+                                            -
+                                          </div>
+                                          <div style={{ width: 140 }}>
+                                            <TimeInput
+                                              name={`qualificationDays.${dayIndex}.details.${detailIndex}.endTime`}
+                                              value={detail.endTime}
+                                              onChange={handleChange}
+                                            />
+                                          </div>
+                                        </div>
+                                      ) : (
+                                        `${detail.startTime} - ${detail.endTime}`
+                                      )}
+                                    </td>
+                                    <td style={{ width: "1%" }}>
+                                      {detail.openEdit ? (
+                                        <div style={{ width: 100 }}>
+                                          <NumberInput
+                                            name={`qualificationDays.${dayIndex}.details.${detailIndex}.quota`}
+                                            value={detail.quota}
+                                            onChange={handleChange}
+                                          />
+                                        </div>
+                                      ) : (
+                                        detail.quota
+                                      )}
+                                    </td>
+                                    <td
+                                      style={{
+                                        width: "1%",
+                                        whiteSpace: "nowrap",
+                                      }}
+                                    >
+                                      {detail.openEdit ? (
+                                        <Button
+                                          type="primary"
+                                          icon="check"
+                                          outline
+                                          size="sm"
+                                          onClick={() =>
+                                            saveSessionItem(
+                                              dayIndex,
+                                              detailIndex
+                                            )
+                                          }
+                                        />
+                                      ) : (
+                                        <Button
+                                          type="warning"
+                                          icon="pencil"
+                                          outline
+                                          size="sm"
+                                          onClick={() =>
+                                            editSessionItem(
+                                              dayIndex,
+                                              detailIndex
+                                            )
+                                          }
+                                        />
+                                      )}{" "}
+                                      <Button
+                                        type="danger"
+                                        icon="trash"
+                                        outline
+                                        size="sm"
+                                        onClick={() =>
+                                          removeSessionItem(
+                                            dayIndex,
+                                            detailIndex
+                                          )
+                                        }
+                                      />
+                                    </td>
+                                  </tr>
+                                ))}
+                            </tbody>
+                          ))}
+                        </table>
+                      </Col>
+                    </Row>
+                  )}
+                </>
               ),
             },
           ]}

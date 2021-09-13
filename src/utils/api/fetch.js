6be87f6ee27f13a0c24038f12f8d3aef2e9837a0
Sulfano Agus Fikri <sulfano.fikri@flash-coffee.com>
2021-09-13 23:41:27 +0700

    handle 401

--- src/utils/api/fetch.js
+++ src/utils/api/fetch.js
@@ -1,15 +1,21 @@
+import { store } from "../../store";
+import * as AuthenticationStore from "store/slice/authentication";
+
 const API_URL = process.env.REACT_APP_API_URL
   ? process.env.REACT_APP_API_URL
-  : "https://api-staging.myarchery.id"
+  : "https://api-staging.myarchery.id";
 
 export default function resource(endpoint, config) {
   return fetch(API_URL + endpoint, config)
-    .then(async response => {
-      const responseObject = await response.json()
-      responseObject.success = response.ok
-      return responseObject
+    .then(async (response) => {
+      const responseObject = await response.json();
+      responseObject.success = response.ok;
+      if (response.status === 401) {
+        store.dispatch(AuthenticationStore.logout());
+      }
+      return responseObject;
     })
-    .catch(error => {
-      return { message: JSON.stringify(error) }
-    })
+    .catch((error) => {
+      return { message: JSON.stringify(error) };
+    });
 }

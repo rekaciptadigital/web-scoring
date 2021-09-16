1e48e6c2bc28cc6517c8b7b80eb1f59f7882b458
putra32 <putralubis1432@gmail.com>
2021-09-16 11:34:38 +0700

    file update

--- src/routes/archer.js
+++ src/routes/archer.js
@@ -1,9 +1,11 @@
 import LoginArcher from "../pages/authArcher/login"
 import RegisterArcher from "../pages/authArcher/register"
+import Logout from "../pages/authArcher/logout"
 
 const archerRouters = [
     { path: "/archer/login", component: LoginArcher, exact: true},
-    { path: "/archer/register", component: RegisterArcher, exact: true}
+    { path: "/archer/register", component: RegisterArcher, exact: true},
+    { path: "/archer/logout", component: Logout, exact: true}
 ]
 
 export default archerRouters

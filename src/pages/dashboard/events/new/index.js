158837cb2f359fa2edf32c74bdd46331b3207fb3
andikapriyotamad <andikapriyotamad@gmail.com>
2021-10-15 22:27:22 +0700

    Layout halaman Pilih Jenis Event

--- src/pages/dashboard/events/new/index.js
+++ src/pages/dashboard/events/new/index.js
@@ -2,16 +2,17 @@ import { Breadcrumbs } from "components";
 import React from "react";
 import { Link } from "react-router-dom";
 import {
+  Container,
+  Col,
+  Row,
   Card,
+  CardHeader,
   CardBody,
   CardImg,
   CardText,
   CardTitle,
-  // Col,
-  Container,
-  // Row,
 } from "reactstrap";
-// import eventFullday from "assets/images/events/fullday.jpg";
+import eventFullday from "assets/images/events/fullday.jpg";
 import eventMarathon from "assets/images/events/marathon.jpg";
 
 const EventsNew = () => {
@@ -23,44 +24,53 @@ const EventsNew = () => {
             title="Events"
             breadcrumbItems={[{ title: "Dashboard" }, { title: "Events" }]}
           />
-          <h2 style={{textAlign: 'center'}}>Pilih Jenis Event Anda</h2>
-          {/* <Row> */}
-            {/* <Col md={12} xl={{size: 3, offset: 3}}>
-              <Card>
-                <CardBody>
-                  <CardTitle className="mt-0">Event Full Day</CardTitle>
-                </CardBody>
-                <CardImg className="img-fluid" src={eventFullday} alt="Skote" />
-                <CardBody>
-                  <CardText>Event diadakan dalam satu satu hari</CardText>
-                  <Link to="/dashboard/events/new/fullday" className="card-link">
-                    Buat Event
-                  </Link>
-                </CardBody>
-              </Card>
-            </Col> */}
-            {/* <Col md={6} xl={3}> */}
-              <Card>
-                <CardBody>
-                  <CardTitle className="mt-0">Event Marathon</CardTitle>
-                </CardBody>
-                <CardImg
-                  className="img-fluid"
-                  src={eventMarathon}
-                  alt="Skote"
-                />
-                <CardBody>
-                  <CardText>
-                    Event diadakan bertahap dalam jangka waktu tertentu mulai
-                    dari tahap kualifikasi hingga eliminasi
-                  </CardText>
-                  <Link to="/dashboard/events/new/marathon" className="card-link">
-                    Buat Event
-                  </Link>
-                </CardBody>
-              </Card>
-            {/* </Col> */}
-          {/* </Row> */}
+
+          <h2 className="text-center">Pilih Jenis Event Anda</h2>
+
+          <Row className="my-5">
+            <Col className="mx-auto" xl={8}>
+              <Row className="gx-5">
+                <Col md={6} xl={6}>
+                  <Card>
+                    <CardHeader>
+                      <CardTitle className="mt-0">Event Full Day</CardTitle>
+                    </CardHeader>
+
+                    <CardImg className="img-fluid p-2" src={eventFullday} alt="Skote" />
+
+                    <CardBody>
+                      <CardText>Event diadakan dalam satu satu hari</CardText>
+
+                      <Link to="/dashboard/events/new/fullday" className="card-link">
+                        Buat Event &rarr;
+                      </Link>
+                    </CardBody>
+                  </Card>
+                </Col>
+
+                <Col md={6} xl={6}>
+                  <Card>
+                    <CardHeader>
+                      <CardTitle className="mt-0">Event Marathon</CardTitle>
+                    </CardHeader>
+
+                    <CardImg className="img-fluid p-2" src={eventMarathon} alt="Skote" />
+
+                    <CardBody>
+                      <CardText>
+                        Event diadakan bertahap dalam jangka waktu tertentu mulai dari tahap
+                        kualifikasi hingga eliminasi
+                      </CardText>
+
+                      <Link to="/dashboard/events/new/marathon" className="card-link">
+                        Buat Event &rarr;
+                      </Link>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
         </Container>
       </div>
     </React.Fragment>

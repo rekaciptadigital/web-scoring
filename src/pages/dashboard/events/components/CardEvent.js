9c5cdc5dc7dade02efb18bfdb472b4e4ca18b661
putra32 <putralubis1432@gmail.com>
2021-10-27 19:11:56 +0700

    file update

--- src/pages/dashboard/events/components/CardEvent.js
+++ src/pages/dashboard/events/components/CardEvent.js
@@ -17,81 +17,33 @@ import {
 
 function CardEvent(props) {
     const [menu, setMenu] = useState(false);
-    const [check, setCheck] = useState(false)
-    const getCheckCopy = () => {
-        navigator.clipboard.writeText(props.detail.event.eventUrl)
-        setCheck(true)
-    }
-    let dStart = new Date(props.detail.event.registrationStartDatetime)
-    let dEnd = new Date(props.detail.event.registrationEndDatetime)
+    // const [check, setCheck] = useState(false)
+    // const getCheckCopy = () => {
+    //     navigator.clipboard.writeText(props.detail.event.eventUrl)
+    //     setCheck(true)
+    // }
+    // let dStart = new Date(props.detail.event.registrationStartDatetime)
+    // let dEnd = new Date(props.detail.event.registrationEndDatetime)
     // console.log(d.toDateString())
 
-    // if (!props.detail) {
-    //     return (
-    //         <dvi>
-    //             <Card className="mini-stats-wid">
-    //             <CardBody>
-    //                 <Row>
-    //                     <Col md={6} sm={12}>
-    //                         <div>
-    //                             <span>
-    //                                 {/* <i className="bx bx-home font-size-24"></i> */}
-    //                                 <img src={satuDashboard} />
-    //                             </span>
-    //                         </div>
-    //                     </Col>
-    //                     <Col md={6} sm={12}>
-    //                         <div>
-    //                         <div className="d-flex justify-content-between">
-    //                             <h4>Jakarta Archery 2021</h4>
-    //                             <Dropdown
-    //                                 isOpen={menu}
-    //                                 toggle={() => setMenu(!menu)}
-    //                             >
-    //                                 <DropdownToggle tag="span">
-    //                                 <h3 className="bx bx-dots-vertical" style={{cursor: 'pointer'}}></h3>
-    //                                 </DropdownToggle>
-    //                                 <DropdownMenu className="dropdown-menu-end">
-    //                                 <DropdownItem tag="a" href="/dashboard/schedule">
-    //                                     {" "}
-    //                                     <span>Lihat Jadwal dan Peserta</span>
-    //                                 </DropdownItem>
-    //                                 </DropdownMenu>
-    //                             </Dropdown>
-    //                         </div>
-    //                         <p className="text-muted fw-medium">
-    //                             13 Agustus - 16 Agustus 2021
-    //                         </p>
-    //                         <p className="text-muted fw-medium">
-    //                             Gelora Bung Karno
-    //                         </p>
-    //                         <Button color="primary">Manage Event</Button>
-    //                         </div>
-    //                     </Col>
-    //                 </Row>
-    //             </CardBody>
-    //         </Card>
-    //         </dvi>
-    //     )
-    // }
-
-    return (
-        <div>
-            <Card className="mini-stats-wid">
+    if (!props.detail) {
+        return (
+            <dvi>
+                <Card className="mini-stats-wid">
                 <CardBody>
                     <Row>
                         <Col md={6} sm={12}>
                             <div>
                                 <span>
                                     {/* <i className="bx bx-home font-size-24"></i> */}
-                                    <img src={props.detail.event.poster ? props.detail.event.poster : satuDashboard} height="120" width="200" />
+                                    <img src={satuDashboard} />
                                 </span>
                             </div>
                         </Col>
                         <Col md={6} sm={12}>
                             <div>
                             <div className="d-flex justify-content-between">
-                                <h4>{props.detail.event.eventName}</h4>
+                                <h4>Jakarta Archery 2021</h4>
                                 <Dropdown
                                     isOpen={menu}
                                     toggle={() => setMenu(!menu)}
@@ -100,39 +52,95 @@ function CardEvent(props) {
                                         <h3 className="bx bx-dots-vertical" style={{cursor: 'pointer'}}></h3>
                                     </DropdownToggle>
                                     <DropdownMenu className="dropdown-menu-end">
-                                        <DropdownItem tag="a" href={"/dashboard/schedule/"+props.detail.event.id}>
+                                        <DropdownItem tag="a" href={"/dashboard/schedule"}>
                                             {" "}
                                             <span>Lihat Jadwal</span>
                                         </DropdownItem>
-                                        <DropdownItem tag="a" href={"/dashboard/member/"+props.detail.event.id}>
+                                        <DropdownItem tag="a" href={"/dashboard/member"}>
                                             {" "}
                                             <span>Lihat Peserta</span>
+                                        </DropdownItem>
+                                        <DropdownItem tag="a" href={"/dashboard/eliminasi"}>
+                                            {" "}
+                                            <span>Setting Eliminasi</span>
                                         </DropdownItem>
                                     </DropdownMenu>
                                 </Dropdown>
                             </div>
                             <p className="text-muted fw-medium">
-                                {dStart != undefined ? dStart.toDateString() : ""} - {dEnd != undefined? dEnd.toDateString():""}
+                                13 Agustus - 16 Agustus 2021
                             </p>
                             <p className="text-muted fw-medium">
-                                {props.detail.event.location} - {props.detail.event.locationType}
+                                Gelora Bung Karno
                             </p>
-                            <div className="mb-2">
-                                <div className="d-flex">
-                                    <a style={{marginRight: '0.5rem'}} target="_blank" rel="noreferrer" href={props.detail.event.eventUrl}>Link Event</a>
-                                    <a onClick={getCheckCopy}>Copy Link</a>
-                                    {!check ? null : (<i className="bx bx-check"></i>)}
-                                </div>
-                                <label style={{color:"green"}}>{props.detail.totalParticipant} peserta terdaftar</label>
-                            </div>
-                                <Button disabled color="primary">Manage Event</Button>
+                            <Button color="primary">Manage Event</Button>
                             </div>
                         </Col>
                     </Row>
                 </CardBody>
             </Card>
-        </div>
-    )
+            </dvi>
+        )
+    }
+
+    // return (
+    //     <div>
+    //         <Card className="mini-stats-wid">
+    //             <CardBody>
+    //                 <Row>
+    //                     <Col md={6} sm={12}>
+    //                         <div>
+    //                             <span>
+    //                                 {/* <i className="bx bx-home font-size-24"></i> */}
+    //                                 <img src={props.detail.event.poster ? props.detail.event.poster : satuDashboard} height="120" width="200" />
+    //                             </span>
+    //                         </div>
+    //                     </Col>
+    //                     <Col md={6} sm={12}>
+    //                         <div>
+    //                         <div className="d-flex justify-content-between">
+    //                             <h4>{props.detail.event.eventName}</h4>
+    //                             <Dropdown
+    //                                 isOpen={menu}
+    //                                 toggle={() => setMenu(!menu)}
+    //                             >
+    //                                 <DropdownToggle tag="span">
+    //                                     <h3 className="bx bx-dots-vertical" style={{cursor: 'pointer'}}></h3>
+    //                                 </DropdownToggle>
+    //                                 <DropdownMenu className="dropdown-menu-end">
+    //                                     <DropdownItem tag="a" href={"/dashboard/schedule/"+props.detail.event.id}>
+    //                                         {" "}
+    //                                         <span>Lihat Jadwal</span>
+    //                                     </DropdownItem>
+    //                                     <DropdownItem tag="a" href={"/dashboard/member/"+props.detail.event.id}>
+    //                                         {" "}
+    //                                         <span>Lihat Peserta</span>
+    //                                     </DropdownItem>
+    //                                 </DropdownMenu>
+    //                             </Dropdown>
+    //                         </div>
+    //                         <p className="text-muted fw-medium">
+    //                             {dStart != undefined ? dStart.toDateString() : ""} - {dEnd != undefined? dEnd.toDateString():""}
+    //                         </p>
+    //                         <p className="text-muted fw-medium">
+    //                             {props.detail.event.location} - {props.detail.event.locationType}
+    //                         </p>
+    //                         <div className="mb-2">
+    //                             <div className="d-flex">
+    //                                 <a style={{marginRight: '0.5rem'}} target="_blank" rel="noreferrer" href={props.detail.event.eventUrl}>Link Event</a>
+    //                                 <a onClick={getCheckCopy}>Copy Link</a>
+    //                                 {!check ? null : (<i className="bx bx-check"></i>)}
+    //                             </div>
+    //                             <label style={{color:"green"}}>{props.detail.totalParticipant} peserta terdaftar</label>
+    //                         </div>
+    //                             <Button disabled color="primary">Manage Event</Button>
+    //                         </div>
+    //                     </Col>
+    //                 </Row>
+    //             </CardBody>
+    //         </Card>
+    //     </div>
+    // )
 }
 
 export default CardEvent

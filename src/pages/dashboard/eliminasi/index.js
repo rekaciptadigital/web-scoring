f48f9efa07b2f63b8f54fd01483a52d77720f4e8
surya darma saqti <surya@stockbit.com>
2021-11-01 01:36:15 +0700

    update eliminasi

--- src/pages/dashboard/eliminasi/index.js
+++ src/pages/dashboard/eliminasi/index.js
@@ -1,52 +1,11 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import MetaTags from "react-meta-tags";
-import {Container, Card, CardBody, Row, Col, Button} from "reactstrap"
+import {Container, Card, CardBody, Row, Col, Button, Input} from "reactstrap"
 import { DateInput, TimeInput, SelectInput } from "components"
-import { Bracket, Seed, SeedItem, SeedTeam } from 'react-brackets'
-import { Link } from "react-router-dom"
-
-const rounds = [
-    {
-      title: 'Round one',
-      seeds: [
-        {
-          id: 1,
-          date: new Date().toDateString(),
-          teams: [{ name: 'Team A', lose: 2 }, { name: 'Team B', lose: 1 }],
-        },
-        {
-          id: 2,
-          date: new Date().toDateString(),
-          teams: [{ name: 'Team C', lose: 1 }, { name: 'Team D', lose : 2 }],
-        },
-        {
-          id: 3,
-          date: new Date().toDateString(),
-          teams: [{ name: 'Team E', lose: 0 }, { name: 'Team F', lose: 0 }],
-        },
-        {
-          id: 4,
-          date: new Date().toDateString(),
-          teams: [{ name: 'Team G', lose: 0 }, { name: 'Team H', lose: 0 }],
-        },
-      ],
-    },
-    {
-      title: 'Round two',
-      seeds: [
-        {
-          id: 5,
-          date: new Date().toDateString(),
-          teams: [{ name: 'Team A' }, { name: 'Team D' }],
-        },
-        {
-          id: 6,
-          date: new Date().toDateString(),
-          teams: [{ name: 'Team E' }, { name: 'Team H' }],
-        },
-      ],
-    },
-  ];
+import { Bracket, Seed, SeedItem, SeedTeam, SeedTime } from 'react-brackets'
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import { EventsService, EliminationService } from "../../../services"
 
   const CustomSeed = ({seed, breakpoint}) => {
     // breakpoint passed to Bracket component
@@ -57,48 +16,128 @@ const rounds = [
       <Seed mobileBreakpoint={breakpoint} style={{ fontSize: 12 }}>
         <SeedItem>
           <div>
-            { seed.teams[0].lose && seed.teams[1].lose ?
-            (
-              <div>
-                <SeedTeam style={{ color: `${seed.teams[0].lose == 1 ? "#757575" : "white"}`, background: `${seed.teams[0].lose == 1 ? "#E2E2E2" : "#BC8B2C"}` }}>{seed.teams[0]?.name || 'NO TEAM '}</SeedTeam>
-                <SeedTeam style={{ color: `${seed.teams[1].lose == 2 ? "white" : "#757575"}`, background: `${seed.teams[1].lose == 2 ? "#BC8B2C" : "#E2E2E2"}`}}>{seed.teams[1]?.name || 'NO TEAM '}</SeedTeam>
-              </div>
-            ):(
-              <div>
-              <SeedTeam>{seed.teams[0]?.name || 'NO TEAM '}</SeedTeam>
-              <SeedTeam>{seed.teams[1]?.name || 'NO TEAM '}</SeedTeam>
-              </div>
-            )
-            }
+              {
+                  seed.teams.map((team) => {
+                    return(
+                        team.win != undefined ? 
+                        team.win == 1 ? 
+                        <SeedTeam style={{ borderBottom: "2px solid black", color: "white", background: "#BC8B2C" }}>{team?.name || "<not have participant>"}</SeedTeam> 
+                        :
+                        <SeedTeam style={{ borderBottom: "2px solid black", color: "#757575", background: "#E2E2E2"}}>{team?.name || "<not have participant>"}</SeedTeam>
+                        :   
+                        <SeedTeam style={{ borderBottom: "2px solid white"}}>{team?.name || '<not have participant>'}</SeedTeam>
+                    )
+                  })
+              }
           </div>
         </SeedItem>
+        <SeedTime>{seed.date}</SeedTime>
       </Seed>
     );
   };
 
 function Eliminasi() {
-    const [eliminasiData, setEliminasiData] = useState([])
+    const [eliminasiSchedule, setEliminasiSchedule] = useState([])
+    const [matches, setMatches] = useState([])
+    const [eventDetail, setEventDetail] = useState({})
+    const [date, setDate] = useState("")
+    const [start, setStart] = useState("")
+    const [end, setEnd] = useState("")
+    const [category, setCategory] = useState(0)
+    const [updated, setUpdated] = useState(true)
+    const [countEliminationMember, setCountEliminationMember] = useState(16)
+    const { event_id } = useParams();
+    const eliminationType = [
+        { id: "1", label: "A vs Z" }, 
+        { id: "2", label: "A vs B" }, 
+        { id: "3", label: "Random" },  
+    ]
+    const genderOptions = [
+        { id: "male", label: "Laki-laki" }, 
+        { id: "female", label: "Perempuan" }, 
+    ]
+    const [type, setType] = useState(eliminationType[0])
+    const [gender, setGender] = useState(genderOptions[0])
 
-    const eliminasiSchedule = () => {
-        return (
-            <div>
-                <div className="d-flex justify-content-between">
-                    <div>
-                        <DateInput label="Tanggal Eliminasi" />
-                    </div>
-                    <div className="ms-1">
-                        <TimeInput label="Jam Mulai" />
-                    </div>
-                    <div className="ms-1">
-                        <TimeInput label="Jam Selesai" />
-                    </div>
-                </div>
-            </div>
-        )
+
+    useEffect(() => {
+        if(eventDetail.id == undefined)
+            getEventDetail()
+
+        setUpdated(true)
+        getEventEliminationTemplate()
+    }, [category, countEliminationMember, type, gender]);
+
+    const getEventDetail = async () => {
+        const {message, errors, data } = await EventsService.getEventById({
+            "id" : event_id
+        })
+        if (data) {
+            console.log("data.categories", data.cetegories)
+            setEventDetail(data)
+            setCategory(data.categories[0])
+            getEliminasiSchedule();
+            console.log(message)
+        } else 
+        console.log(message)
+        console.log(errors)
     }
 
-    const addEliminasiSchedule = () => {
-        setEliminasiData([...eliminasiData, eliminasiSchedule()])
+    const getEventEliminationTemplate = async () => {
+        const {message, errors, data } = await EliminationService.getEventEliminationTemplate({
+            "event_id" : event_id,
+            "match_type" : type.id,
+            "gender" : gender.id,
+            "event_category_id" : category.id,
+            "elimination_member_count" : countEliminationMember
+        })
+        if (data) {
+            setMatches(data);
+        } else 
+        console.log(message)
+        console.log(errors)
+    }
+
+    const addEliminasiSchedule = async() => {
+        const {message, errors, data } = await EliminationService.setEventEliminationSchedule({
+            "event_id" : event_id,
+            "date" : date,
+            "start_time" : start,
+            "end_time" : end,
+        })
+        if (data) {
+            setDate("");
+            setEnd("");
+            setStart("");
+            getEliminasiSchedule();
+        } else 
+        console.log(message)
+        console.log(errors)
+    }
+
+    const setEliminasi = async() => {
+        const {message, errors, data } = await EliminationService.setEventElimination({
+            "match_type" : type.id,
+            "gender" : gender.id,
+            "event_category_id" : category.id,
+            "elimination_member_count" : countEliminationMember
+        })
+        if (data) {
+            getEventEliminationTemplate();
+        } else 
+        console.log(message)
+        console.log(errors)
+    }
+
+    const getEliminasiSchedule = async() => {
+        const {message, errors, data } = await EliminationService.getEventEliminationSchedule({
+            "event_id" : event_id,
+        })
+        if (data) {
+            setEliminasiSchedule(data)
+        } else 
+        console.log(message)
+        console.log(errors)
     }
 
     const dummyOptions = [
@@ -108,7 +147,6 @@ function Eliminasi() {
         { id: "1", label: "Babak 4" }, 
     ]
 
-    console.log(eliminasiData)
     return (
         <React.Fragment>
             <div className="page-content">
@@ -123,13 +161,34 @@ function Eliminasi() {
                                 <span className="ms-2">Dashboard</span>
                             </div>
                         </Link>
-                        <h3>Setting Match Eliminasi</h3>
+                        <h3>Setting Match Eliminasi {"'"+eventDetail.eventName+"'"}</h3>
                         <Row>
                             <Col md={7}>
                                 <Card>
                                     <CardBody>
                                         <div>
-                                            <Bracket rounds={rounds} renderSeedComponent={CustomSeed} />
+                                            <SelectInput label ={"kategori"} options={eventDetail.categories} value={category} placeholder="select" onChange={(e)=>{setCategory(e.value)}} />
+                                        </div>
+                                        <div>
+                                            <label>jumlah peserta eliminasi</label>
+                                            <br></br>
+                                            <Input readOnly={updated == true ? true : true} style={{width:"70px"}} type="number" value={countEliminationMember} onChange={(e)=>{setCountEliminationMember(e.target.value)}} />
+                                        </div>
+                                        <div>
+                                            <SelectInput readOnly={true} style={{width:"100px"}} label ={"type"} options={eliminationType} value={type} placeholder="select type" onChange={(e)=>{setType(e.value)}} />
+                                        </div>
+                                        <div>
+                                            <SelectInput style={{width:"200px"}} label ={"jenis kelamin"} options={genderOptions} value={gender} placeholder="select type" onChange={(e)=>{setGender(e.value)}} />
+                                        </div>
+                                        <div className="float-end mt-2">
+                                            <Button disabled={matches.updated ? false : true} onClick={setEliminasi}>Terapkan</Button>
+                                        </div>
+                                    </CardBody>
+                                </Card>
+                                <Card>
+                                    <CardBody style={{overflow: "auto"}}>
+                                        <div>
+                                            <Bracket rounds={matches.rounds != undefined ? matches.rounds : []} renderSeedComponent={CustomSeed} />
                                         </div>
                                     </CardBody>
                                 </Card>
@@ -138,32 +197,30 @@ function Eliminasi() {
                             <Card>
                                     <CardBody>
                                         <div>
-                                            <div className="d-flex justify-content-between">
-                                                <h5>Elimination member count</h5>
-                                                <div className="border-1">00</div>
-                                            </div>
                                             <div>
-                                                <p>Jadwal Eliminasi</p>
+                                                <h5>Setting Jadwal Eliminasi</h5>
                                                  <div style={{height: '220px', overflowY: 'auto'}}>
                                                     <div className="d-flex justify-content-between">
                                                         <div>
-                                                            <DateInput label="Tanggal" />
+                                                            <DateInput value={date} onChange={(e)=>{setDate(e.value)}} label="Tanggal" />
                                                         </div>
                                                         <div>
-                                                            <TimeInput label="Jam Mulai" />
+                                                            <TimeInput value={start} onChange={(e)=>{setStart(e.value)}} label="Jam Mulai" />
                                                         </div>
                                                         <div>
-                                                            <TimeInput label="Jam Selesai" />
+                                                            <TimeInput value={end} onChange={(e)=>{setEnd(e.value)}} label="Jam Selesai" />
                                                         </div>
                                                         <div style={{paddingTop: '1.6rem'}} className="ms-1">
                                                             <Button onClick={addEliminasiSchedule}>+</Button>
                                                         </div>
                                                     </div>
-                                                    {eliminasiData}
+                                                    <br></br>
+                                                    {
+                                                        eliminasiSchedule.map((d)=>{
+                                                            return (<p key={d.id}>+ {d.date+" ["+d.startTime+" - "+d.endTime+"]"}</p>);
+                                                        })
+                                                    }
                                                 </div>
-                                            </div>
-                                            <div className="float-end mt-2">
-                                                <Button>Terapkan</Button>
                                             </div>
                                         </div>
                                     </CardBody>
@@ -174,32 +231,16 @@ function Eliminasi() {
                                             <div>
                                                 <Row>
                                                     <Col>
-                                                        <h5>Babak</h5>
-                                                    </Col>
-                                                    <Col>
-                                                    <div>
-                                                        <SelectInput options={dummyOptions} placeholder="Babak 1" />
-                                                    </div>
+                                                        <h5>Setting Match And Schedule</h5>
                                                     </Col>
                                                 </Row>
                                             </div>
                                             <div style={{height: '220px', overflowY: 'scroll'}}>
                                                 <div className="w-75">
-                                                    <div className="mt-2">
-                                                        <SelectInput label="Sesi 1" />
+                                                    <div>
+                                                        <SelectInput options={dummyOptions} placeholder="Babak 1" />
                                                     </div>
-                                                    <div className="mt-2">
-                                                        <SelectInput label="Sesi 2" />
-                                                    </div>
-                                                    <div className="mt-2">
-                                                        <SelectInput label="Sesi 3" />
-                                                    </div>
-                                                    <div className="mt-2">
-                                                        <SelectInput label="Sesi 4" />
-                                                    </div>
-                                                    <div className="mt-2">
-                                                        <SelectInput label="Sesi 5" />
-                                                    </div>
+                                                    
                                                 </div>
                                             </div>
                                             <div className="float-end mt-2">

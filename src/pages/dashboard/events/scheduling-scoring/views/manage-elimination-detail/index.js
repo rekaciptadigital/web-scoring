2f9cdc910979a335a8c50728b4ac3deab55a8ef8
andikapriyotamad <andikapriyotamad@gmail.com>
2022-03-02 09:45:47 +0700

    Pewarnaan seed pemenang tidak dimunculkan di bagan preview

--- src/pages/dashboard/events/scheduling-scoring/views/manage-elimination-detail/index.js
+++ src/pages/dashboard/events/scheduling-scoring/views/manage-elimination-detail/index.js
@@ -257,7 +257,7 @@ function SeedBagan({ bracketProps, configs, onSuccess }) {
               key={index}
               className={classnames({
                 "item-active": shouldEnableScoring(),
-                "item-winner": parseInt(team.win) === 1 && !isBye,
+                "item-winner": configs.isSettingApplied && parseInt(team.win) === 1 && !isBye,
               })}
             >
               <BoxName>{team.name || "-"}</BoxName>

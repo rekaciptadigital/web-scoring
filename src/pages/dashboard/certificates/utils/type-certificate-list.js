589921ac565745e78f4cc14a10ec4863faf215ad
suryadarmasaqti19 <suryadarmasaqti19@gmail.com>
2022-03-16 09:52:25 +0700

    update label kode

--- src/pages/dashboard/certificates/utils/type-certificate-list.js
+++ src/pages/dashboard/certificates/utils/type-certificate-list.js
@@ -1,17 +1,19 @@
 const optionsTypeCertificate = [
   { label: "Peserta", value: 1 },
-  { label: "Peserta Eliminasi", value: 3 },
+  // { label: "Peserta Eliminasi", value: 3 },
   { label: "Juara Kualifikasi", value: 4 },
   { label: "Juara Eliminasi", value: 2 },
   { label: "Juara Beregu", value: 5 },
+  { label: "Juara Beregu Campuran", value: 5 },
 ];
 
 const label = {
   1: "Peserta",
   2: "Juara Eliminasi",
-  3: "Peserta Eliminasi",
-  4: "Juara Kualifikasi",
+  // 3: "Peserta Eliminasi",
+  4: "Kualifikasi",
   5: "Juara Beregu",
+  6: "Juara Beregu Campuran",
 };
 
 function getCurrentTypeCertificate(typeCertificate) {

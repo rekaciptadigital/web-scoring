41b2077e796151ac4ecebb2ec2d9bcabb442ff8b
andikapriyotamad <andikapriyotamad@gmail.com>
2022-04-04 19:04:39 +0700

    File boilterplate buat di-replace

---
+++ src/pages/dashboard/events/budrests/index.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+import { SubNavbar } from "../components/submenus-settings";
+
+function PageEventBudRests() {
+  const eventId = 22;
+  return (
+    <div>
+      <SubNavbar eventId={eventId} />
+    </div>
+  );
+}
+
+export default PageEventBudRests;

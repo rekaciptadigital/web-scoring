94edb22906156879b6df779c13d36d8dcd17719e
andikapriyotamad <andikapriyotamad@gmail.com>
2022-07-01 22:15:15 +0700

    Routing url tanpa segmen "kualifikasi"

--- src/routes/live-scores.js
+++ src/routes/live-scores.js
@@ -1,9 +1,8 @@
 import PageLiveScore from "pages/live-scores-qualification";
 
 const liveScoreRoutes = [
-  { path: "/live-score/:event_id/qualification", component: PageLiveScore },
-  // TODO:
-  // { path: "/live-score/:event_id/elimination", component: PageLiveScoreElimination },
+  // routes
+  { path: "/live-score/:event_id", component: PageLiveScore },
 ];
 
 export default liveScoreRoutes;

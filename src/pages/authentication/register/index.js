ee2ba0093f07df51c0d05261cc1b9936f88ae72d
andikapriyotamad <andikapriyotamad@gmail.com>
2022-07-11 19:16:14 +0700

    Form registrasi 2 screen

--- src/pages/authentication/register/index.js
+++ src/pages/authentication/register/index.js
@@ -1,48 +1,48 @@
-// availity-reactstrap-validation
-import myachery from "assets/images/myachery/logo 3.png"
-import React, { useEffect, useState } from "react"
-// import google from "assets/images/myachery/Google.png"
-// import facebook from "assets/images/myachery/Facebook.png"
-// import ladBg from "assets/images/myachery/achery-lad.png"
-import { AvField, AvForm } from "availity-reactstrap-validation"
-import MetaTags from "react-meta-tags"
-import { useDispatch, useSelector } from "react-redux"
-import { useHistory, Link } from "react-router-dom"
-import { AuthenticationService } from "services"
-import { Col, Container, Row, Card, CardBody } from "reactstrap"
-import * as AuthenticationStore from "store/slice/authentication"
-import toastr from "toastr"
-// import logoImg from "../../../assets/images/logo.svg"
-// import images
-// import profileImg from "../../../assets/images/profile-img.png"
+import React, { useEffect } from "react";
+import { useHistory, Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import * as AuthenticationStore from "store/slice/authentication";
+import { AuthenticationService } from "services";
+import toastr from "toastr";
+
+import MetaTags from "react-meta-tags";
+import { Col, Container, Row, Card, CardBody } from "reactstrap";
+import { AvField, AvForm } from "availity-reactstrap-validation";
+import { ButtonBlue, ButtonGhostBlue } from "components/ma";
+import { ProcessingToast, toast } from "./components/processing-toast";
+import { SelectInfoSource } from "./components/select-info-source";
+import { SelectCity } from "./components/select-city";
+import { SelectProvince } from "./components/select-province";
+
+import IconLeft from "components/ma/icons/mono/arrow-left";
+
+import myachery from "assets/images/myachery/logo 3.png";
 
 const Register = () => {
-  const [registerErrors, setRegisterErrors] = useState()
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { isLoggedIn } = useSelector(AuthenticationStore.getAuthenticationStore);
+  const [screen, setScreen] = React.useState(0);
+  const [provinceId, setProvinceId] = React.useState();
+  const [formStep1, setFormStep1] = React.useState(null);
 
-  const dispatch = useDispatch()
-  const { isLoggedIn } = useSelector(AuthenticationStore.getAuthenticationStore)
-  let history = useHistory();
-  
-  const handleValidSubmit = async (event, values) => {
-    const { data, errors, message, success } = await AuthenticationService.register(values)
+  const handleValidSubmit = async (values) => {
+    const payload = _makePayload(formStep1, values);
+    const { data, message, success } = await AuthenticationService.register(payload);
     if (success) {
       if (data) {
-        dispatch(AuthenticationStore.register())
+        dispatch(AuthenticationStore.register());
       }
     } else {
-      console.log(errors)
-      setRegisterErrors(errors)
-      toastr.error(message)
+      toastr.error(message);
     }
-  }
+  };
 
   useEffect(() => {
     if (isLoggedIn) {
-      history.push("/")
+      history.push("/");
     }
-  }, [isLoggedIn])
-
-  console.log(registerErrors)
+  }, [isLoggedIn]);
 
   return (
     <React.Fragment>
@@ -56,6 +56,7 @@ const Register = () => {
       </div>
       <div className="account-pages my-5 pt-sm-5">
         <Container>
+          <ProcessingToast />
           <Row className="justify-content-center">
             <Col md={8} lg={6} xl={5}>
               <Card className="overflow-hidden">
@@ -64,12 +65,12 @@ const Register = () => {
                     <Col xs={7}>
                       <div className="text-light p-4">
                         <h5 className="text-light">Daftar MyArchery.id</h5>
-                        <p>Get your free MyArchery.id <br />account now.</p>
+                        <p>
+                          Get your free MyArchery.id <br />
+                          account now.
+                        </p>
                       </div>
                     </Col>
-                    {/* <Col className="col-5 align-self-end">
-                      <img src={profile} alt="" className="img-fluid" />
-                    </Col> */}
                   </Row>
                 </div>
                 <CardBody className="pt-0">
@@ -77,110 +78,208 @@ const Register = () => {
                     <Link to="/" className="auth-logo-light">
                       <div className="avatar-md profile-user-wid mb-4">
                         <span className="avatar-title rounded-circle bg-light">
-                          <img
-                            src={myachery}
-                            alt=""
-                            className="rounded-circle"
-                            height="34"
-                          />
+                          <img src={myachery} alt="" className="rounded-circle" height="34" />
                         </span>
                       </div>
                     </Link>
                   </div>
-                  <div className="p-2">
-                  <AvForm
-              className="form-horizontal"
-              onValidSubmit={(e, v) => {
-                handleValidSubmit(e, v)
-              }}
-              >
-                <div className="mb-2">
-                  <AvField
-                    name="email"
-                    label="Email"
-                    className="form-control"
-                    placeholder="Masukkan email/Nama EO Terdaftar"
-                    type="email"
-                    required
-                  />
-                </div>
-                <div className="mb-2">
-                  <AvField
-                    name="name"
-                    label="Nama EO"
-                    className="form-control"
-                    placeholder="Masukkan email/Nama EO Terdaftar"
-                    type="text"
-                    required
-                  />
-                </div>
-                <div className="mb-2">
-                        <AvField
-                          name="password"
-                          label="Kata Sandi"
-                          type="password"
-                          required
-                          placeholder="Masukkan kata sandi"
-                        />
-                      </div>
-                <div className="mb-2">
-                  <AvField
-                    name="password_confirmation"
-                    label="Konfirmasi Kata Sandi"
-                    type="password"
-                    required
-                    placeholder="Masukkan kata sandi"
-                  />
-                </div>
-                <div className="mt-3 d-grid">
-                        <button
-                          className="btn btn-primary btn-block"
-                          type="submit"
-                        >
-                          Register
-                        </button>
-                      </div>
-
-                      {/* <div className="text-center mt-4">
-                        <p className="font-size-14 color-black">Atau daftar dengan</p>
-                        <div>
-                          <img src={facebook} alt="" />
-                          <img src={google} alt="" />
+                  {screen === 0 ? (
+                    <div className="p-2" key="step-1">
+                      <AvForm
+                        className="form-horizontal"
+                        onValidSubmit={(e, values) => {
+                          toast.success("Lanjut ke langkah kedua");
+                          setFormStep1(values);
+                          setScreen(1);
+                        }}
+                      >
+                        <div className="mb-3">
+                          <AvField
+                            name="name"
+                            label="Nama Penyelengara"
+                            className="form-control"
+                            placeholder="Masukkan nama"
+                            type="text"
+                            required
+                            errorMessage="Nama penyelenggara wajib diisi"
+                            defaultValue={formStep1?.name}
+                          />
                         </div>
-                      </div> */}
+                        <div className="mb-3">
+                          <AvField
+                            name="email"
+                            label="Email"
+                            className="form-control"
+                            placeholder="Masukkan email (digunakan untuk masuk)"
+                            type="email"
+                            required
+                            errorMessage="Email wajib diisi"
+                            defaultValue={formStep1?.email}
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <AvField
+                            name="phone"
+                            label="Nomor Telepon"
+                            className="form-control"
+                            placeholder="Masukkan nomor telepon (nomor WhatsApp)"
+                            type="text"
+                            required
+                            errorMessage="Nomor telepon wajib diisi"
+                            defaultValue={formStep1?.phone}
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <AvField
+                            name="password"
+                            label="Kata Sandi"
+                            type="password"
+                            required
+                            placeholder="Masukkan kata sandi"
+                            errorMessage="Kata sandi wajib diisi"
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <AvField
+                            name="password_confirmation"
+                            label="Konfirmasi Kata Sandi"
+                            type="password"
+                            required
+                            placeholder="Masukkan ulang kata sandi di atas"
+                            errorMessage="Kata sandi wajib dikonfirmasi"
+                          />
+                        </div>
 
-                      <div className="mt-4 text-center">
-                        <Link
-                          to="/forgot-password"
-                          className="text-muted"
-                        >
-                          <i className="mdi mdi-lock me-1" />
-                          Forgot your password?
-                        </Link>
+                        <div className="mt-3 d-grid">
+                          <ButtonBlue type="submit">Selanjutnya</ButtonBlue>
+                        </div>
+                      </AvForm>
+                    </div>
+                  ) : screen === 1 ? (
+                    <div className="p-2" key="step-2">
+                      <div className="mb-3">
+                        <ButtonGhostBlue flexible onClick={() => setScreen(0)}>
+                          <IconLeft size="14" /> Kembali
+                        </ButtonGhostBlue>
                       </div>
-              </AvForm>
-                  </div>
+
+                      <AvForm
+                        className="form-horizontal"
+                        onValidSubmit={(e, v) => handleValidSubmit(v)}
+                      >
+                        <div className="mb-3">
+                          <AvField
+                            name="province"
+                            label="Provinsi"
+                            className="form-control"
+                            placeholder="Pilih provinsi"
+                            tag={SelectProvince}
+                            onChange={(value) => setProvinceId(value)}
+                            required
+                            errorMessage="Provinsi wajib diisi"
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <AvField
+                            name="city"
+                            label="Kota"
+                            className="form-control"
+                            placeholder="Pilih kota"
+                            provinceId={provinceId}
+                            tag={SelectCity}
+                            required
+                            errorMessage="Kota wajib diisi"
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <AvField
+                            name="questionnaire_source"
+                            label="Dari mana Anda mengetahui MyArchery?"
+                            placeholder="Pilih opsi"
+                            type="text"
+                            tag={SelectInfoSource}
+                            required
+                            errorMessage="Silakan isi sesuai pertanyaan di atas"
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <AvField
+                            name="questionnaire_motive"
+                            label="Mengapa Anda ingin menggunakan MyArchery?"
+                            className="form-control"
+                            placeholder="Sebutkan alasan"
+                            type="text"
+                            required
+                            errorMessage="Silakan isi sesuai pertanyaan di atas"
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <AvField
+                            name="questionnaire_event_name"
+                            label="Event terdekat apa yang ingin Anda selenggarakan?"
+                            className="form-control"
+                            placeholder="Sebutkan nama event"
+                            type="text"
+                            required
+                            errorMessage="Silakan isi sesuai pertanyaan di atas"
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <AvField
+                            name="questionnaire_event_description"
+                            label="Deskripsikan event yang akan Anda selenggarakan?"
+                            className="form-control"
+                            placeholder="Masukkan deskripsi singkat"
+                            type="textarea"
+                            required
+                            errorMessage="Silakan isi sesuai pertanyaan di atas"
+                          />
+                        </div>
+
+                        <div className="mt-3 d-grid">
+                          <ButtonBlue type="submit">Buat Akun</ButtonBlue>
+                        </div>
+                      </AvForm>
+                    </div>
+                  ) : (
+                    <div className="p-2">Tidak ada screen</div>
+                  )}
                 </CardBody>
               </Card>
               <div className="mt-5 text-center">
                 <p>
-                  Already have an account ?{" "}
+                  Sudah punya akun?{" "}
                   <Link to="/login" className="fw-medium text-primary">
-                    {" "}
-                    Login{" "}
-                  </Link>{" "}
+                    Login
+                  </Link>
                 </p>
-                {/* <p>
-                  © {new Date().getFullYear()} Skote. Crafted with{" "}
-                  <i className="mdi mdi-heart text-danger" /> by Themesbrand
-                </p> */}
               </div>
             </Col>
           </Row>
         </Container>
       </div>
     </React.Fragment>
-  )
+  );
+};
+
+function _makePayload(formStep1, values) {
+  return {
+    // step 1
+    name_organizer: formStep1.name,
+    email: formStep1.email,
+    password: formStep1.password,
+    password_confirmation: formStep1.password_confirmation,
+    phone_number: formStep1.phone,
+    // step 2
+    province_id: values.province,
+    city_id: values.city,
+    intro: {
+      where: values.questionnaire_source,
+      why: values.questionnaire_motive,
+      what: values.questionnaire_event_name,
+      description: values.questionnaire_event_description,
+    },
+  };
 }
 
-export default Register
+export default Register;

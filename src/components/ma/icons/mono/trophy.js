70033ddff151e6e53753c504f31266ff35594c62
andikapriyotamad <andikapriyotamad@gmail.com>
2022-08-12 20:02:11 +0700

    Komponen page wrapper

---
+++ src/components/ma/icons/mono/trophy.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function IconTrophy({ size = 24 }) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6.745 4H17.313C17.313 4 16.433 17.257 12.029 17.257C9.879 17.257 8.568 14.093 7.79 10.857C6.976 7.468 6.745 4 6.745 4Z"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M17.313 4C17.313 4 18.234 3.017 19 3C20.5 2.966 20.777 4 20.777 4C21.071 4.61 21.306 6.194 19.897 7.657C18.487 9.12 16.91 10.4 16.268 10.857"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.745 3.99998C6.745 3.99998 5.785 3.00598 5 2.99998C3.5 2.98798 3.223 3.99998 3.223 3.99998C2.929 4.60998 2.694 6.19398 4.103 7.65698C5.2427 8.82238 6.47581 9.89262 7.79 10.857"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.50699 20C8.50699 18.171 12.029 17.257 12.029 17.257C12.029 17.257 15.552 18.171 15.552 20H8.50699Z"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default IconTrophy;

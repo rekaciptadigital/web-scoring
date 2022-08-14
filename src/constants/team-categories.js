0e898e2810e594421fad2f1aa7edb9745d90a449
andikapriyotamad <andikapriyotamad@gmail.com>
2022-08-15 04:52:37 +0700

    Toolbar filter kategori reusable & configurable

---
+++ src/constants/team-categories.js
@@ -0,0 +1,19 @@
+const TEAM_LABELS = {
+  "individu male": "Individu Putra",
+  "individu female": "Individu Putri",
+  individu_mix: "Individu",
+  male_team: "Beregu Putra",
+  female_team: "Beregu Putri",
+  mix_team: "Beregu Campuran",
+};
+
+const TEAM_IDS_IN_FIXED_ORDER = [
+  "individu male",
+  "individu female",
+  "individu_mix",
+  "male_team",
+  "female_team",
+  "mix_team",
+];
+
+export default { TEAM_LABELS, TEAM_IDS_IN_FIXED_ORDER };

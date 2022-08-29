da9ffa717afae99305d989183780219aad729459
andikapriyotamad <andikapriyotamad@gmail.com>
2022-08-29 13:43:33 +0700

    Screen step 4 untuk atur pemeringkatan klub & formnya

---
+++ src/pages/dashboard/events/new/screens/rules/index.js
@@ -0,0 +1,189 @@
+import * as React from "react";
+import styled from "styled-components";
+import { useFormRankingSetting } from "./hooks/form-ranking-settings";
+
+import { ButtonBlue } from "components/ma";
+import { FieldInputTextSmall } from "pages/dashboard/events/components/form-fields";
+import { SelectRadio } from "../../components/select-radio";
+import { FieldSelectCategories } from "./components/field-select-categories";
+import { FieldSelectOption } from "./components/field-select-option";
+
+function ScreenRules({ eventDetail }) {
+  return (
+    <CardSheet>
+      <Section>
+        <SettingsClubsRanking eventDetail={eventDetail} />
+      </Section>
+    </CardSheet>
+  );
+}
+
+function SettingsClubsRanking({ eventDetail }) {
+  const { isDirty, data, errors, updateField, setType, handleValidation } = useFormRankingSetting();
+  const { type, rankingName, categories, medalCountingType } = data;
+
+  const categoryDetails = eventDetail?.eventCategories;
+  const optionsCategories = React.useMemo(() => {
+    return categoryDetails?.map((cat) => ({
+      value: cat.categoryDetailsId,
+      label: cat.label,
+      data: cat,
+    }));
+  }, [categoryDetails]);
+
+  return (
+    <SettingContainer>
+      <SpacedVertical>
+        <div>
+          <h5>Aturan Pemeringkatan Klub</h5>
+          <p>
+            Pemeringkatan klub merupakan skor yang diperingkatkan berdasarkan perolehan skor dari
+            masing-masing klub dan akan ditampilkan pada menu &#34;Pemeringkatan Klub&#34; pada tiap
+            event. Anda dapat menampilkan semua kategori saja atau dapat mengatur kategori tertentu
+            untuk ditampilkan.
+          </p>
+        </div>
+
+        <div>
+          <SelectRadio
+            options={[
+              { value: 1, label: "Semua Kategori" },
+              { value: 2, label: "Kategori Tertentu" },
+              { value: 3, label: "Gabungan dari Kategori Tertentu (Khusus)" },
+            ]}
+            value={type}
+            onChange={(value) => setType(parseInt(value))}
+          />
+        </div>
+
+        {type > 1 && (
+          <React.Fragment>
+            {type === 3 && (
+              <div>
+                <FieldInputTextSmall
+                  label="Penamaan Pemeringkatan"
+                  placeholder="Masukkan nama"
+                  required
+                  value={rankingName}
+                  onChange={(ev) => updateField("rankingName", ev.target.value)}
+                  errors={errors.rankingName}
+                  isTouched // prop legacy dari form input bantalan
+                />
+              </div>
+            )}
+
+            <div>
+              <FieldSelectCategories
+                label="Kategori"
+                placeholder="Pilih opsi"
+                required
+                options={optionsCategories}
+                value={categories}
+                onChange={(value) => updateField("categories", value || [])}
+                errors={errors.categories}
+              />
+              <SubtleFieldNote>
+                Kategori pemeringkatan klub akan ditampilkan satu-satu sesuai kategori yang dipilih.
+                Anda dapat memilih lebih dari satu kategori untuk ditampilkan pada menu
+                &#34;Pemeringkatan Klub&#34;
+              </SubtleFieldNote>
+            </div>
+
+            <div>
+              <FieldSelectOption
+                label="Perhitungan Medali"
+                placeholder="Pilih opsi"
+                required
+                options={[
+                  { value: 1, label: "Digabung" },
+                  { value: 2, label: "Dipisah" },
+                ]}
+                value={medalCountingType}
+                onChange={(value) => updateField("medalCountingType", value)}
+                errors={errors.medalCountingType}
+              />
+              <SubtleFieldNote>
+                Perhitungan medali pada kategori tertentu dapat &#34;digabung&#34; atau
+                &#34;dipisah&#34;. Dimana &#34;digabung&#34; merupakan jumlah medali dari kategori
+                yang telah dipilih dan ditotalkan pada &#34;Semua Kategori&#34;. Sedangkan
+                &#34;dipisah&#34; merupakan medali yang diperoleh dari masing-masing kategori dan
+                tidak dihitung / digabung pada &#34;Semua Kategori&#34;.
+              </SubtleFieldNote>
+            </div>
+          </React.Fragment>
+        )}
+
+        <div>
+          <BottomActions>
+            <ButtonBlue
+              title={!isDirty && "Ubah pengaturan untuk menyimpan"}
+              disabled={!isDirty}
+              onClick={() =>
+                handleValidation({
+                  onInvalid: (errors) => {
+                    // TODO: ke depan bisa kasih toast untuk display pesan error
+                    Object.values(errors).forEach((error) =>
+                      error.forEach((message) => console.error(message))
+                    );
+                  },
+                  onValid: () => {
+                    // TODO: submit
+                  },
+                })
+              }
+            >
+              Simpan
+            </ButtonBlue>
+          </BottomActions>
+        </div>
+      </SpacedVertical>
+    </SettingContainer>
+  );
+}
+
+/* ============================ */
+// styles
+
+const CardSheet = styled.div`
+  margin-bottom: 24px;
+
+  padding: 35px;
+  border: 0 solid #f6f6f6;
+  border-radius: 8px;
+  background-color: #ffffff;
+  background-clip: border-box;
+  box-shadow: 0 0.75rem 1.5rem rgb(18 38 63 / 3%);
+`;
+
+const Section = styled.section`
+  border: 1px solid var(--ma-gray-200);
+  border-radius: 0.5rem;
+  background-clip: border-box;
+`;
+
+const SettingContainer = styled.div`
+  padding: 1.25rem;
+`;
+
+const SpacedVertical = styled.div`
+  > * + * {
+    margin-top: 1.5rem;
+  }
+`;
+
+const SubtleFieldNote = styled.div`
+  margin-top: 0.5rem;
+  color: var(--ma-gray-600);
+  font-size: 0.9em;
+`;
+
+const BottomActions = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 0.5rem;
+`;
+
+/* ============================ */
+// utils
+
+export { ScreenRules };
--- src/pages/dashboard/events/new/utils/last-unlocked-step.js
+++ src/pages/dashboard/events/new/utils/last-unlocked-step.js
@@ -1,4 +1,7 @@
-function computeLastUnlockedStep(sortedFormStatusList) {
+function computeLastUnlockedStep(emptyStatusByStepNumber) {
+  const sortedFormStatusList = Object.keys(emptyStatusByStepNumber).map(
+    (number) => emptyStatusByStepNumber[number]
+  );
   let stepSequenceNumber = 0;
   for (const isEmpty of sortedFormStatusList) {
     stepSequenceNumber = stepSequenceNumber + 1;

fb08f2b3f777913a4da32cf1e883bd28fb389ecf
faizadie30 <faiz.adie30@gmail.com>
2023-04-06 17:22:26 +0700

    fixing

--- src/pages/dashboard/events/new/screens/categories.js
+++ src/pages/dashboard/events/new/screens/categories.js
@@ -8,7 +8,12 @@ import {
 import { useSubmitCategories } from "../hooks/submit-categories";
 
 import SweetAlert from "react-bootstrap-sweetalert";
-import { Button, ButtonBlue, ButtonOutlineBlue, AlertSubmitError } from "components/ma";
+import {
+  Button,
+  ButtonBlue,
+  ButtonOutlineBlue,
+  AlertSubmitError,
+} from "components/ma";
 import { FieldSelectSmall } from "../../components/form-fields";
 import { LoadingScreen } from "../components/loading-screen-portal";
 import { toast } from "../components/processing-toast";
@@ -21,7 +26,6 @@ import illustrationAlertPublication from "assets/images/events/alert-publication
 
 function ScreenCategories({ eventDetail, fetchEventDetail, form, formFees }) {
   const { data, setMaxLengthFromOptions } = form;
-
   const { options: optionsCompetitionCategories } = useCompetitionCategories();
   const { options: optionsAgeCategories } = useAgeCategories();
   const { options: optionsDistanceCategories } = useDistanceCategories();
@@ -41,20 +45,22 @@ function ScreenCategories({ eventDetail, fetchEventDetail, form, formFees }) {
   return (
     <div>
       {Boolean(data?.length) &&
-        data.map((category) => (
-          <CategoriesByCompetition
-            key={category.key}
-            eventDetail={eventDetail}
-            fetchEventDetail={fetchEventDetail}
-            form={form}
-            formFees={formFees}
-            category={category}
-            defaultCompetitionCategory={unselectedCompetitionCategories[0]}
-            optionsCompetitionCategories={unselectedCompetitionCategories}
-            optionsAgeCategories={optionsAgeCategories}
-            optionsDistanceCategories={optionsDistanceCategories}
-          />
-        ))}
+        data.map((category) => {
+          return (
+            <CategoriesByCompetition
+              key={category.key}
+              eventDetail={eventDetail}
+              fetchEventDetail={fetchEventDetail}
+              form={form}
+              formFees={formFees}
+              category={category}
+              defaultCompetitionCategory={unselectedCompetitionCategories[0]}
+              optionsCompetitionCategories={unselectedCompetitionCategories}
+              optionsAgeCategories={optionsAgeCategories}
+              optionsDistanceCategories={optionsDistanceCategories}
+            />
+          );
+        })}
     </div>
   );
 }
@@ -186,95 +192,110 @@ function CategoriesByCompetition({
       </CompetitionCategorySelector>
 
       <VerticalSpacedBoxLoose>
-        {category.categoryDetails.map((detail) => (
-          <CategoryBlock key={detail.key}>
-            <ClassGroup>
-              <VerticalSpacedBox>
-                <PairedFields>
-                  <FieldSelectClass
-                    name={`ageCategoryId-${detail.key}`}
-                    options={optionsAgeCategories}
-                    value={detail.ageCategoryId}
-                    onChange={(value) => {
-                      selectCategoryDetailItem("ageCategoryId", category.key, detail.key, value);
-                    }}
-                  >
-                    Kelas
-                  </FieldSelectClass>
+        {category.categoryDetails.map((detail) => {
+          return (
+            <CategoryBlock key={detail.key}>
+              <ClassGroup>
+                <VerticalSpacedBox>
+                  <PairedFields>
+                    <FieldSelectClass
+                      name={`ageCategoryId-${detail.key}`}
+                      options={optionsAgeCategories}
+                      value={detail.ageCategoryId}
+                      onChange={(value) => {
+                        selectCategoryDetailItem(
+                          "ageCategoryId",
+                          category.key,
+                          detail.key,
+                          value
+                        );
+                      }}
+                    >
+                      Kelas
+                    </FieldSelectClass>
 
-                  <FieldSelectSmall
-                    name={`distanceId-${detail.key}`}
-                    options={optionsDistanceCategories}
-                    value={detail.distanceId}
-                    onChange={(value) => {
-                      selectCategoryDetailItem("distanceId", category.key, detail.key, value);
-                    }}
-                  >
-                    Jarak
-                  </FieldSelectSmall>
-                </PairedFields>
+                    <FieldSelectSmall
+                      name={`distanceId-${detail.key}`}
+                      options={optionsDistanceCategories}
+                      value={detail.distanceId}
+                      onChange={(value) => {
+                        selectCategoryDetailItem(
+                          "distanceId",
+                          category.key,
+                          detail.key,
+                          value
+                        );
+                      }}
+                    >
+                      Jarak
+                    </FieldSelectSmall>
+                  </PairedFields>
 
-                <div>
-                  <h6 className="fw-bold">Detail Kategori</h6>
-                  <DetailQuotaGrid>
-                    {detail.quotas.map((quota) => {
-                      const teamName = getTeamName(quota.teamCategoryId);
-                      const isDisabled = !checkIsTeamActive(teamName);
-                      return (
-                        <div key={quota.teamCategoryId}>
-                          <FieldInputQuota
-                            name={`quota-${quota.teamCategoryId}-${detail.key}`}
-                            placeholder="kuota"
-                            disabled={isDisabled}
-                            value={quota.quota || ""}
-                            onChange={(ev) => {
-                              setQuotaAmount(
-                                quota.teamCategoryId,
-                                category.key,
-                                detail.key,
-                                ev.target.value
-                              );
-                            }}
-                          >
-                            {quota.teamCategoryLabel}
-                          </FieldInputQuota>
-                        </div>
-                      );
-                    })}
-                  </DetailQuotaGrid>
-                </div>
-              </VerticalSpacedBox>
-            </ClassGroup>
+                  <div>
+                    <h6 className="fw-bold">Detail Kategori</h6>
+                    <DetailQuotaGrid>
+                      {detail.quotas.map((quota) => {
+                        const teamName = getTeamName(quota.teamCategoryId);
+                        const isDisabled = !checkIsTeamActive(teamName);
+                        return (
+                          <div key={quota.teamCategoryId}>
+                            <FieldInputQuota
+                              name={`quota-${quota.teamCategoryId}-${detail.key}`}
+                              placeholder="kuota"
+                              disabled={isDisabled}
+                              value={quota.quota || ""}
+                              onChange={(ev) => {
+                                setQuotaAmount(
+                                  quota.teamCategoryId,
+                                  category.key,
+                                  detail.key,
+                                  ev.target.value
+                                );
+                              }}
+                            >
+                              {quota.teamCategoryLabel}
+                            </FieldInputQuota>
+                          </div>
+                        );
+                      })}
+                    </DetailQuotaGrid>
+                  </div>
+                </VerticalSpacedBox>
+              </ClassGroup>
 
-            <div>
-              <Button flexible onClick={() => createEmptyCategoryDetail(category.key)}>
-                <IconPlus size="13" />
-              </Button>
+              <div>
+                <Button
+                  flexible
+                  onClick={() => createEmptyCategoryDetail(category.key)}
+                >
+                  <IconPlus size="13" />
+                </Button>
 
-              <ButtonWithConfirmPrompt
-                flexible
-                disabled={category.categoryDetails.length <= 1}
-                messagePrompt="Ingin hapus kategori?"
-                buttonConfirmLabel="Hapus"
-                onConfirm={() => {
-                  if (!detail.isAlive) {
-                    removeCategoryDetailByKey(category.key, detail.key);
-                  } else {
-                    deleteByCategoryClass(detail, {
-                      eventId: eventDetail?.id,
-                      onSuccess() {
-                        toast.success("Berhasil menghapus kategori");
-                        fetchEventDetail();
-                      },
-                    });
-                  }
-                }}
-              >
-                <IconTrash size="13" />
-              </ButtonWithConfirmPrompt>
-            </div>
-          </CategoryBlock>
-        ))}
+                <ButtonWithConfirmPrompt
+                  flexible
+                  disabled={category.categoryDetails.length <= 1}
+                  messagePrompt="Ingin hapus kategori?"
+                  buttonConfirmLabel="Hapus"
+                  onConfirm={() => {
+                    if (!detail.isAlive) {
+                      removeCategoryDetailByKey(category.key, detail.key);
+                    } else {
+                      deleteByCategoryClass(detail, {
+                        eventId: eventDetail?.id,
+                        onSuccess() {
+                          toast.success("Berhasil menghapus kategori");
+                          fetchEventDetail();
+                        },
+                      });
+                    }
+                  }}
+                >
+                  <IconTrash size="13" />
+                </ButtonWithConfirmPrompt>
+              </div>
+            </CategoryBlock>
+          );
+        })}
 
         <div>
           <ButtonWithConfirmPrompt
@@ -359,15 +380,24 @@ function ButtonWithConfirmPrompt({
         onCancel={handleCancel}
         style={{ width: 800, padding: "35px 88px", borderRadius: "1.25rem" }}
         customButtons={
-          <span className="d-flex justify-content-center" style={{ gap: "0.5rem", width: "100%" }}>
-            <Button onClick={handleCancel}>{buttonCancelLabel || "Batal"}</Button>
-            <ButtonBlue onClick={handleConfirm}>{buttonConfirmLabel || "Konfirmasi"}</ButtonBlue>
+          <span
+            className="d-flex justify-content-center"
+            style={{ gap: "0.5rem", width: "100%" }}
+          >
+            <Button onClick={handleCancel}>
+              {buttonCancelLabel || "Batal"}
+            </Button>
+            <ButtonBlue onClick={handleConfirm}>
+              {buttonConfirmLabel || "Konfirmasi"}
+            </ButtonBlue>
           </span>
         }
       >
         <IllustationAlertPrompt />
         {messagePrompt && <h4>{messagePrompt}</h4>}
-        {messageDescription && <p className="text-muted">{messageDescription}</p>}
+        {messageDescription && (
+          <p className="text-muted">{messageDescription}</p>
+        )}
       </SweetAlert>
     </React.Fragment>
   );
@@ -402,7 +432,9 @@ const getUnselectedCompetitionCategories = (data, options) => {
     return [];
   }
 
-  const selectedIds = data.map((category) => category.competitionCategoryId?.value);
+  const selectedIds = data.map(
+    (category) => category.competitionCategoryId?.value
+  );
 
   let unselected = [];
   for (const option of options) {
